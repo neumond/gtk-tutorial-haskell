a3528b2e5e8bec0bca545fc1bb160037b7c182aa
neumond <knifeslaughter@gmail.com>
2014-04-13 01:53:50 +0400

    example 2 works

--- gtkwin.hs
+++ gtkwin.hs
@@ -5,12 +5,23 @@ main :: IO()
 
 deleteEventHandler = do
     putStrLn "Delete event occured"
+    return True
+
+destroyEventHandler = do
     mainQuit
-    return False
+
+helloClick = do
+    putStrLn "Hello World!"
 
 main = do
     initGUI
     window <- windowNew
     on window deleteEvent $ liftIO deleteEventHandler
+    on window objectDestroy $ destroyEventHandler
+    containerSetBorderWidth window 10
+    button <- buttonNewWithLabel "Hello world"
+    on button buttonActivated $ helloClick
+    on button buttonActivated $ widgetDestroy window
+    containerAdd window button
     widgetShowAll window
     mainGUI
