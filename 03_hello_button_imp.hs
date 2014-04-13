b358878f07d65ab47623a9d7120d2ccac10f83a9
neumond <knifeslaughter@gmail.com>
2014-04-13 04:42:09 +0400

    3rd example

---
+++ 03_hello_button_imp.hs
@@ -0,0 +1,42 @@
+import Graphics.UI.Gtk
+import Control.Monad.Trans(liftIO)
+
+main :: IO()
+
+helloAgainCallback b = do
+    putStrLn $ "Hello again - " ++ show b ++ " was pressed"
+
+deleteEventHandler = do
+    mainQuit
+    return False
+
+main = do
+    initGUI
+    window <- windowNew
+    windowSetTitle window "Hello Buttons!"
+    on window deleteEvent $ liftIO deleteEventHandler
+    containerSetBorderWidth window 10
+
+    box1 <- hBoxNew False 0
+    containerAdd window box1
+
+    button <- buttonNewWithLabel "Button 1"
+    on button buttonActivated $ helloAgainCallback "button 1"
+    boxPackStart box1 button PackGrow 0
+    widgetShow button
+
+    -- Haskell     | (expand,fill) C
+    -- -----------------------------
+    -- PackGrow    = (True,True)
+    -- PackRepel   = (True,False)
+    -- PackNatural = (False,False)
+
+    button <- buttonNewWithLabel "Button 2"
+    on button buttonActivated $ helloAgainCallback "button 2"
+    boxPackStart box1 button PackGrow 0
+    widgetShow button
+
+    widgetShow box1
+    widgetShow window
+
+    mainGUI
